3cc3e14b42a8181881467996a2aa65959848a88f
Salekin Imran <abir045@gmail.com>
2021-01-18 14:28:10 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+//components
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+
+
+const App = () => {
+  return (
+    <div>
+      <MyNavbar/>
+      <h1>My portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
